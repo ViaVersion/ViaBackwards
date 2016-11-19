04508e8c5c75ae6407837a315ed13d82b3ee63a9
Matsv <mats@matsv.nl>
2016-11-19 10:57:55 +0100

    Back to snapshot

--- all/build.sh
+++ all/build.sh
@@ -5,5 +5,6 @@ echo "Start maven build" &&
 mvn clean install &&
 echo "Copying" &&
 cd ./all/ &&
-cp ./target/viabackwards-all-2.0-DEV.jar /home/adr/mc/Spigot/TestServers/BungeeCord/plugins/ &&
+cp ./target/viabackwards-all-2.0.jar /home/adr/mc/Spigot/TestServers/BungeeCord/plugins/ &&
+cp ./target/viabackwards-all-2.0.jar /home/adr/mc/Spigot/TestServers/1.11/plugins/ &&
 echo "Done."
