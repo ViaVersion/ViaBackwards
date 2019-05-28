afe7c69ec1057dbb2979c0862718cadf1d43fd70
KennyTV <28825609+KennyTV@users.noreply.github.com>
2019-05-28 13:11:31 +0200

    1.14.2

--- core/src/main/java/nl/matsv/viabackwards/api/ViaBackwardsPlatform.java
+++ core/src/main/java/nl/matsv/viabackwards/api/ViaBackwardsPlatform.java
@@ -20,6 +20,7 @@ import nl.matsv.viabackwards.protocol.protocol1_12to1_12_1.Protocol1_12To1_12_1;
 import nl.matsv.viabackwards.protocol.protocol1_13_1to1_13_2.Protocol1_13_1To1_13_2;
 import nl.matsv.viabackwards.protocol.protocol1_13_2to1_14.Protocol1_13_2To1_14;
 import nl.matsv.viabackwards.protocol.protocol1_13to1_13_1.Protocol1_13To1_13_1;
+import nl.matsv.viabackwards.protocol.protocol1_14_1to1_14_2.Protocol1_14_1To1_14_2;
 import nl.matsv.viabackwards.protocol.protocol1_14to1_14_1.Protocol1_14To1_14_1;
 import nl.matsv.viabackwards.protocol.protocol1_9_4to1_10.Protocol1_9_4To1_10;
 import us.myles.ViaVersion.api.protocol.ProtocolRegistry;
@@ -48,6 +49,7 @@ public interface ViaBackwardsPlatform {
             ProtocolRegistry.registerProtocol(new Protocol1_13_1To1_13_2(), Collections.singletonList(ProtocolVersion.v1_13_1.getId()), ProtocolVersion.v1_13_2.getId());
             ProtocolRegistry.registerProtocol(new Protocol1_13_2To1_14(), Collections.singletonList(ProtocolVersion.v1_13_2.getId()), ProtocolVersion.v1_14.getId());
             ProtocolRegistry.registerProtocol(new Protocol1_14To1_14_1(), Collections.singletonList(ProtocolVersion.v1_14.getId()), ProtocolVersion.v1_14_1.getId());
+            ProtocolRegistry.registerProtocol(new Protocol1_14_1To1_14_2(), Collections.singletonList(ProtocolVersion.v1_14_1.getId()), ProtocolVersion.v1_14_2.getId());
         }
     }
 
---
+++ core/src/main/java/nl/matsv/viabackwards/protocol/protocol1_14_1to1_14_2/Protocol1_14_1To1_14_2.java
@@ -0,0 +1,15 @@
+package nl.matsv.viabackwards.protocol.protocol1_14_1to1_14_2;
+
+import nl.matsv.viabackwards.api.BackwardsProtocol;
+import us.myles.ViaVersion.api.data.UserConnection;
+
+public class Protocol1_14_1To1_14_2 extends BackwardsProtocol {
+
+    @Override
+    protected void registerPackets() {
+    }
+
+    @Override
+    public void init(UserConnection userConnection) {
+    }
+}
