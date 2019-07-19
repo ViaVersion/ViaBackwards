d66eb0c56bdccee301bec7cccc6bc25ddafa9f3b
KennyTV <28825609+KennyTV@users.noreply.github.com>
2019-07-19 12:53:37 +0200

    1.14.4 support

---
+++ core/src/main/java/nl/matsv/viabackwards/protocol/protocol1_14_3to1_14_4/Protocol1_14_3To1_14_4.java
@@ -0,0 +1,60 @@
+package nl.matsv.viabackwards.protocol.protocol1_14_3to1_14_4;
+
+import nl.matsv.viabackwards.api.BackwardsProtocol;
+import us.myles.ViaVersion.api.PacketWrapper;
+import us.myles.ViaVersion.api.data.UserConnection;
+import us.myles.ViaVersion.api.remapper.PacketHandler;
+import us.myles.ViaVersion.api.remapper.PacketRemapper;
+import us.myles.ViaVersion.api.type.Type;
+import us.myles.ViaVersion.packets.State;
+
+public class Protocol1_14_3To1_14_4 extends BackwardsProtocol {
+
+    @Override
+    protected void registerPackets() {
+        // Acknowledge Player Digging - added in pre4
+        registerOutgoing(State.PLAY, 0x5c, -1, new PacketRemapper() {
+            @Override
+            public void registerMap() {
+                handler(new PacketHandler() {
+                    @Override
+                    public void handle(PacketWrapper wrapper) throws Exception {
+                        wrapper.cancel();
+                    }
+                });
+            }
+        });
+
+        // Trade list
+        registerOutgoing(State.PLAY, 0x27, 0x27, new PacketRemapper() {
+            @Override
+            public void registerMap() {
+                handler(new PacketHandler() {
+                    @Override
+                    public void handle(PacketWrapper wrapper) throws Exception {
+                        wrapper.passthrough(Type.VAR_INT);
+                        int size = wrapper.passthrough(Type.UNSIGNED_BYTE);
+                        for (int i = 0; i < size; i++) {
+                            wrapper.passthrough(Type.FLAT_VAR_INT_ITEM);
+                            wrapper.passthrough(Type.FLAT_VAR_INT_ITEM);
+                            if (wrapper.passthrough(Type.BOOLEAN)) {
+                                wrapper.passthrough(Type.FLAT_VAR_INT_ITEM);
+                            }
+                            wrapper.passthrough(Type.BOOLEAN);
+                            wrapper.passthrough(Type.INT);
+                            wrapper.passthrough(Type.INT);
+                            wrapper.passthrough(Type.INT);
+                            wrapper.passthrough(Type.INT);
+                            wrapper.passthrough(Type.FLOAT);
+                            wrapper.read(Type.INT); // demand value added in pre-5
+                        }
+                    }
+                });
+            }
+        });
+    }
+
+    @Override
+    public void init(UserConnection userConnection) {
+    }
+}
