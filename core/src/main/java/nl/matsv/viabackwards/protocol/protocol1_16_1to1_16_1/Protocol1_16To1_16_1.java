b79bf429f7aa86ca22ac15f6ef859bcdc0fe3f9e
KennyTV <kennytv@t-online.de>
2020-06-24 14:16:21 +0200

    1.16.1

---
+++ core/src/main/java/nl/matsv/viabackwards/protocol/protocol1_16_1to1_16_1/Protocol1_16To1_16_1.java
@@ -0,0 +1,12 @@
+package nl.matsv.viabackwards.protocol.protocol1_16_1to1_16_1;
+
+import nl.matsv.viabackwards.api.BackwardsProtocol;
+import us.myles.ViaVersion.protocols.protocol1_16to1_15_2.ClientboundPackets1_16;
+import us.myles.ViaVersion.protocols.protocol1_16to1_15_2.ServerboundPackets1_16;
+
+public class Protocol1_16To1_16_1 extends BackwardsProtocol<ClientboundPackets1_16, ClientboundPackets1_16, ServerboundPackets1_16, ServerboundPackets1_16> {
+
+    public Protocol1_16To1_16_1() {
+        super(ClientboundPackets1_16.class, ClientboundPackets1_16.class, ServerboundPackets1_16.class, ServerboundPackets1_16.class);
+    }
+}
