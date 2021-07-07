f8d10be0319d23f17fe9fa7cc402a90470cb2a7c
KennyTV <jahnke.nassim@gmail.com>
2021-07-07 10:08:35 +0200

    Bump version to 4.0.2-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 allprojects {
     group = "com.viaversion"
-    version = "4.0.1"
+    version = "4.0.2-SNAPSHOT"
     description = "Allow older clients to join newer server versions."
 }
 
