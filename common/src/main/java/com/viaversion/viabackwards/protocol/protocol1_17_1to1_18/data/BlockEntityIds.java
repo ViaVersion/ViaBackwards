1ef1a861ca268a62dca3184ff713673aa7399d8c
Nassim Jahnke <jahnke.nassim@gmail.com>
2021-09-16 18:31:03 +0200

    Fix block entity ids

--- common/src/main/java/com/viaversion/viabackwards/protocol/protocol1_17_1to1_18/data/BlockEntityIds.java
+++ common/src/main/java/com/viaversion/viabackwards/protocol/protocol1_17_1to1_18/data/BlockEntityIds.java
@@ -9,8 +9,12 @@ public final class BlockEntityIds {
     static {
         final int[] ids = com.viaversion.viaversion.protocols.protocol1_18to1_17_1.BlockEntityIds.getIds();
         IDS = new int[Arrays.stream(ids).max().getAsInt() + 1];
+        Arrays.fill(IDS, -1);
         for (int i = 0; i < ids.length; i++) {
-            IDS[ids[i]] = i;
+            final int id = ids[i];
+            if (id != -1) {
+                IDS[id] = i;
+            }
         }
     }
 
