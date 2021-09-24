04a970ef089cd27b816ec8177a36f58e22c40dda
Nassim Jahnke <jahnke.nassim@gmail.com>
2021-09-24 14:39:19 +0200

    21w38a

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 allprojects {
     group = "com.viaversion"
-    version = "4.1.0-21w37a-SNAPSHOT"
+    version = "4.1.0-21w38a-SNAPSHOT"
     description = "Allow older clients to join newer server versions."
 }
 
