304b21425c65db9694ae601d07913846430e4244
Nassim Jahnke <jahnke.nassim@gmail.com>
2022-07-26 18:48:10 +0200

    4.4.0 Release

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 allprojects {
     group = "com.viaversion"
-    version = "4.4.0-1.19.1-rc3-SNAPSHOT"
+    version = "4.4.0"
     description = "Allow older clients to join newer server versions."
 }
 
