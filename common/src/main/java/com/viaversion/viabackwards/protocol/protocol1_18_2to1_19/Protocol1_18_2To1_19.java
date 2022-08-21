98304168b841d719d11865cc3f1ae81316ef03b5
Pieter12345 <Pieter12345@users.noreply.github.com>
2022-08-21 09:03:58 +0200

    Fix 1.18.2->1.19 CHAT_MESSAGE using static timestamp (#495)

--- common/src/main/java/com/viaversion/viabackwards/protocol/protocol1_18_2to1_19/Protocol1_18_2To1_19.java
+++ common/src/main/java/com/viaversion/viabackwards/protocol/protocol1_18_2to1_19/Protocol1_18_2To1_19.java
@@ -234,7 +234,7 @@ public final class Protocol1_18_2To1_19 extends BackwardsProtocol<ClientboundPac
             @Override
             public void registerMap() {
                 map(Type.STRING); // Message
-                create(Type.LONG, Instant.now().toEpochMilli()); // Timestamp
+                handler(wrapper -> wrapper.write(Type.LONG, Instant.now().toEpochMilli())); // Timestamp
                 create(Type.LONG, 0L); // Salt
                 handler(wrapper -> {
                     final String message = wrapper.get(Type.STRING, 0);
