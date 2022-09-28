c55402710800c0cbefd57699d4c8979d78aa7946
Nassim Jahnke <nassim@njahnke.dev>
2022-09-28 20:10:26 +0200

    Change windswept_forest biome mapping

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 allprojects {
     group = "com.viaversion"
-    version = "4.4.2"
+    version = "4.4.3-SNAPSHOT"
     description = "Allow older clients to join newer server versions."
 }
 
