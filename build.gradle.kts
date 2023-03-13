94f8cb662b91d0d36a0b9f7c548efe6cc874c30f
Nassim Jahnke <nassim@njahnke.dev>
2023-03-13 10:51:00 +0100

    4.6.0 Release

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 allprojects {
     group = "com.viaversion"
-    version = "4.6.0-1.19.4-pre4-SNAPSHOT"
+    version = "4.6.0"
     description = "Allow older clients to join newer server versions."
 }
 
