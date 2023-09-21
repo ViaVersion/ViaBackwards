57ca5f1bf5d8000c1aac3ee490b0c85bdbf35b2f
Nassim Jahnke <nassim@njahnke.dev>
2023-09-21 10:35:42 +1000

    Fix particle handling

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,7 +15,7 @@ dependencyResolutionManagement {
 
 pluginManagement {
     plugins {
-        id("net.kyori.blossom") version "2.0.1"
+        id("net.kyori.blossom") version "2.1.0"
         id("org.jetbrains.gradle.plugin.idea-ext") version "1.1.7"
         id("com.github.johnrengelman.shadow") version "8.1.1"
     }
