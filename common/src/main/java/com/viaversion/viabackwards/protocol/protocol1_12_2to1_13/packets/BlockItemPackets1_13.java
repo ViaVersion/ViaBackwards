e6966ea42420db40a5b3039187efba5c13731f01
FlorianMichael <60033407+FlorianMichael@users.noreply.github.com>
2024-01-29 17:19:05 +0100

    Fixed wrong item type usages

--- common/src/main/java/com/viaversion/viabackwards/protocol/protocol1_12_2to1_13/packets/BlockItemPackets1_13.java
+++ common/src/main/java/com/viaversion/viabackwards/protocol/protocol1_12_2to1_13/packets/BlockItemPackets1_13.java
@@ -237,7 +237,7 @@ public class BlockItemPackets1_13 extends com.viaversion.viabackwards.api.rewrit
             @Override
             public void register() {
                 map(Type.UNSIGNED_BYTE);
-                map(Type.ITEM1_13_ARRAY, Type.ITEM1_8_SHORT_ARRAY);
+                map(Type.ITEM1_13_SHORT_ARRAY, Type.ITEM1_8_SHORT_ARRAY);
 
                 handler(itemArrayToClientHandler(Type.ITEM1_8_SHORT_ARRAY));
             }
