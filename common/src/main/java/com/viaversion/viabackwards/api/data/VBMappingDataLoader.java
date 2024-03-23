9a500b981b893f2ac2e90f005d09813640e646e8
FlorianMichael <60033407+FlorianMichael@users.noreply.github.com>
2024-03-23 22:39:50 +0100

    Use MappingDataLoader for VBMappingDataLoader

--- common/src/main/java/com/viaversion/viabackwards/api/data/VBMappingDataLoader.java
+++ common/src/main/java/com/viaversion/viabackwards/api/data/VBMappingDataLoader.java
@@ -18,39 +18,20 @@
 package com.viaversion.viabackwards.api.data;
 
 import com.viaversion.viabackwards.ViaBackwards;
-import com.viaversion.viaversion.libs.gson.JsonIOException;
-import com.viaversion.viaversion.libs.gson.JsonObject;
-import com.viaversion.viaversion.libs.gson.JsonSyntaxException;
+import com.viaversion.viaversion.api.data.MappingDataLoader;
 import com.viaversion.viaversion.libs.opennbt.tag.builtin.CompoundTag;
 import com.viaversion.viaversion.libs.opennbt.tag.builtin.Tag;
-import com.viaversion.viaversion.libs.opennbt.tag.io.NBTIO;
-import com.viaversion.viaversion.libs.opennbt.tag.io.TagReader;
-import com.viaversion.viaversion.util.GsonUtil;
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.Map;
 import org.checkerframework.checker.nullness.qual.Nullable;
 
-public class VBMappingDataLoader {
-
-    private static final TagReader<CompoundTag> TAG_READER = NBTIO.reader(CompoundTag.class).named();
+public class VBMappingDataLoader extends MappingDataLoader {
 
     public static final VBMappingDataLoader INSTANCE = new VBMappingDataLoader();
 
-    public @Nullable CompoundTag loadNBT(final String name) {
-        final InputStream resource = getResource(name);
-        if (resource == null) {
-            return null;
-        }
-
-        try (final InputStream stream = resource) {
-            return TAG_READER.read(stream);
-        } catch (final IOException e) {
-            throw new RuntimeException(e);
-        }
+    public VBMappingDataLoader() {
+        super(VBMappingDataLoader.class, "assets/viabackwards/data/");
     }
 
     /**
@@ -70,7 +51,7 @@ public class VBMappingDataLoader {
 
         ViaBackwards.getPlatform().getLogger().info("Loading " + name + " from plugin folder");
         try {
-            final CompoundTag fileData = TAG_READER.read(file.toPath(), false);
+            final CompoundTag fileData = MAPPINGS_READER.read(file.toPath(), false);
             return mergeTags(packedData, fileData);
         } catch (final IOException e) {
             throw new RuntimeException(e);
@@ -91,42 +72,5 @@ public class VBMappingDataLoader {
             original.put(entry.getKey(), entry.getValue());
         }
         return original;
-    }
-
-    public JsonObject loadData(final String name) {
-        try (final InputStream stream = getResource(name)) {
-            if (stream == null) return null;
-            return GsonUtil.getGson().fromJson(new InputStreamReader(stream), JsonObject.class);
-        } catch (final IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public JsonObject loadFromDataDir(final String name) {
-        final File file = new File(ViaBackwards.getPlatform().getDataFolder(), name);
-        if (!file.exists()) {
-            return loadData(name);
-        }
-
-        // Load the file from the platform's directory if present
-        try (final FileReader reader = new FileReader(file)) {
-            return GsonUtil.getGson().fromJson(reader, JsonObject.class);
-        } catch (final JsonSyntaxException e) {
-            ViaBackwards.getPlatform().getLogger().warning(name + " is badly formatted!");
-            e.printStackTrace();
-            ViaBackwards.getPlatform().getLogger().warning("Falling back to resource's file!");
-            return loadData(name);
-        } catch (final IOException | JsonIOException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    /**
-     * Returns the resource as an input stream.
-     * Platforms can override this method to load resources from their own directories by creating a new instance of this class.
-     */
-    public @Nullable InputStream getResource(final String name) {
-        return VBMappingDataLoader.class.getClassLoader().getResourceAsStream("assets/viabackwards/data/" + name);
     }
 }
