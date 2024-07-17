b6caa30c163ab35b1a96ecbbffb770903c97d88b
EnZaXD <florian.michael07@gmail.com>
2024-07-17 19:06:44 +0200

    Add alternative sculk shrieker mapping in 1.19->1.18.2 (#820)

--- common/src/main/java/com/viaversion/viabackwards/protocol/v1_19to1_18_2/data/BackwardsMappingData1_19.java
+++ common/src/main/java/com/viaversion/viabackwards/protocol/v1_19to1_18_2/data/BackwardsMappingData1_19.java
@@ -17,6 +17,7 @@
  */
 package com.viaversion.viabackwards.protocol.v1_19to1_18_2.data;
 
+import com.viaversion.viabackwards.ViaBackwards;
 import com.viaversion.viabackwards.api.data.BackwardsMappingData;
 import com.viaversion.viabackwards.api.data.BackwardsMappingDataLoader;
 import com.viaversion.viaversion.libs.fastutil.ints.Int2ObjectMap;
@@ -29,6 +30,8 @@ import org.checkerframework.checker.nullness.qual.Nullable;
 
 public final class BackwardsMappingData1_19 extends BackwardsMappingData {
 
+    private final boolean sculkShriekerToCryingObsidian = ViaBackwards.getConfig().sculkShriekerToCryingObsidian();
+
     private final Int2ObjectMap<CompoundTag> defaultChatTypes = new Int2ObjectOpenHashMap<>();
 
     public BackwardsMappingData1_19() {
@@ -39,11 +42,26 @@ public final class BackwardsMappingData1_19 extends BackwardsMappingData {
     protected void loadExtras(final CompoundTag data) {
         super.loadExtras(data);
 
+        if (sculkShriekerToCryingObsidian) {
+            blockMappings.setNewId(850, 750);
+            for (int i = 18900; i <= 18907; i++) {
+                blockStateMappings.setNewId(i, 16082);
+            }
+        }
+
         final ListTag<CompoundTag> chatTypes = BackwardsMappingDataLoader.INSTANCE.loadNBT("chat-types-1.19.1.nbt").getListTag("values", CompoundTag.class);
         for (final CompoundTag chatType : chatTypes) {
             final NumberTag idTag = chatType.getNumberTag("id");
             defaultChatTypes.put(idTag.asInt(), chatType);
         }
+    }
+
+    @Override
+    public int getNewItemId(final int id) {
+        if (sculkShriekerToCryingObsidian && id == 329) {
+            return 1065;
+        }
+        return super.getNewItemId(id);
     }
 
     public @Nullable CompoundTag chatType(final int id) {
