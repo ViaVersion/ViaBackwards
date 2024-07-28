9fed88c1a3b9dde85ed670f589c5c42a8e740f00
Nassim Jahnke <nassim@njahnke.dev>
2024-07-28 14:38:59 +0200

    5.0.2 Release

--- common/src/main/java/com/viaversion/viabackwards/api/ViaBackwardsPlatform.java
+++ common/src/main/java/com/viaversion/viabackwards/api/ViaBackwardsPlatform.java
@@ -69,7 +69,7 @@ import java.util.logging.Logger;
 
 public interface ViaBackwardsPlatform {
 
-    String MINIMUM_VV_VERSION = "5.0.1";
+    String MINIMUM_VV_VERSION = "5.0.2";
 
     /**
      * Initialize ViaBackwards.
