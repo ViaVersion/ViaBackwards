90878517db8ed6ec4a44c100fa2e89aed27f09a3
RK_01 <50594595+RaphiMC@users.noreply.github.com>
2024-10-22 10:26:05 +0200

    Fix CLIENT_INFORMATION skin flags data type (#4)

--- common/src/main/java/com/viaversion/viabackwards/protocol/v1_21_2to1_21/Protocol1_21_2To1_21.java
+++ common/src/main/java/com/viaversion/viabackwards/protocol/v1_21_2to1_21/Protocol1_21_2To1_21.java
@@ -139,7 +139,7 @@ public final class Protocol1_21_2To1_21 extends BackwardsProtocol<ClientboundPac
         wrapper.passthrough(Types.BYTE); // View distance
         wrapper.passthrough(Types.VAR_INT); // Chat visibility
         wrapper.passthrough(Types.BOOLEAN); // Chat colors
-        wrapper.passthrough(Types.BYTE); // Skin parts
+        wrapper.passthrough(Types.UNSIGNED_BYTE); // Skin parts
         wrapper.passthrough(Types.VAR_INT); // Main hand
         wrapper.passthrough(Types.BOOLEAN); // Text filtering enabled
         wrapper.passthrough(Types.BOOLEAN); // Allow listing
