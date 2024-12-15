c770c3e1fa57bcff61269dfbcda372ccf05b6d6a
Nassim Jahnke <nassim@njahnke.dev>
2024-12-15 10:13:52 +0100

    Update VV dep, a field type changes

--- common/src/main/java/com/viaversion/viabackwards/api/ViaBackwardsPlatform.java
+++ common/src/main/java/com/viaversion/viabackwards/api/ViaBackwardsPlatform.java
@@ -72,7 +72,7 @@ import java.util.logging.Logger;
 
 public interface ViaBackwardsPlatform {
 
-    String MINIMUM_VV_VERSION = "5.2.0";
+    String MINIMUM_VV_VERSION = "5.2.1";
 
     /**
      * Initialize ViaBackwards.
