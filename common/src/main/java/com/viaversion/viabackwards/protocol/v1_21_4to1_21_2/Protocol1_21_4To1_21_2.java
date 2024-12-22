b197c758b5031ceb4fdb25862263cff80f9426ae
Nassim Jahnke <nassim@njahnke.dev>
2024-12-22 10:54:05 +0100

    Add empty flowers and tall_flowers tags Fixes #945

--- common/src/main/java/com/viaversion/viabackwards/protocol/v1_21_4to1_21_2/Protocol1_21_4To1_21_2.java
+++ common/src/main/java/com/viaversion/viabackwards/protocol/v1_21_4to1_21_2/Protocol1_21_4To1_21_2.java
@@ -27,6 +27,7 @@ import com.viaversion.viabackwards.protocol.v1_21_4to1_21_2.rewriter.EntityPacke
 import com.viaversion.viabackwards.protocol.v1_21_4to1_21_2.rewriter.ParticleRewriter1_21_4;
 import com.viaversion.viaversion.api.connection.UserConnection;
 import com.viaversion.viaversion.api.minecraft.Particle;
+import com.viaversion.viaversion.api.minecraft.RegistryType;
 import com.viaversion.viaversion.api.minecraft.entities.EntityTypes1_21_4;
 import com.viaversion.viaversion.api.protocol.packet.provider.PacketTypesProvider;
 import com.viaversion.viaversion.api.protocol.packet.provider.SimplePacketTypesProvider;
@@ -163,6 +164,13 @@ public final class Protocol1_21_4To1_21_2 extends BackwardsProtocol<ClientboundP
                 }
             }
         });
+    }
+
+    @Override
+    protected void onMappingDataLoaded() {
+        super.onMappingDataLoaded();
+        tagRewriter.addEmptyTags(RegistryType.ITEM, "minecraft:tall_flowers", "minecraft:flowers");
+        tagRewriter.addEmptyTag(RegistryType.BLOCK, "minecraft:tall_flowers");
     }
 
     @Override
