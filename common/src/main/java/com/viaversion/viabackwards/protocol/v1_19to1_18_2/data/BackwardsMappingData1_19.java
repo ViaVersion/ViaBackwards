e0f3a98d79e375e2b0d984b4570e286f4d0b29c0
FlorianMichael <florian.michael07@gmail.com>
2025-06-12 15:10:11 +0200

    Don't add block mappings for alternative mappings (#1024)

--- common/src/main/java/com/viaversion/viabackwards/protocol/v1_19to1_18_2/data/BackwardsMappingData1_19.java
+++ common/src/main/java/com/viaversion/viabackwards/protocol/v1_19to1_18_2/data/BackwardsMappingData1_19.java
@@ -17,20 +17,18 @@
  */
 package com.viaversion.viabackwards.protocol.v1_19to1_18_2.data;
 
+import com.viaversion.nbt.tag.CompoundTag;
+import com.viaversion.nbt.tag.ListTag;
+import com.viaversion.nbt.tag.NumberTag;
 import com.viaversion.viabackwards.ViaBackwards;
 import com.viaversion.viabackwards.api.data.BackwardsMappingData;
 import com.viaversion.viabackwards.api.data.BackwardsMappingDataLoader;
 import com.viaversion.viaversion.libs.fastutil.ints.Int2ObjectMap;
 import com.viaversion.viaversion.libs.fastutil.ints.Int2ObjectOpenHashMap;
-import com.viaversion.nbt.tag.CompoundTag;
-import com.viaversion.nbt.tag.ListTag;
-import com.viaversion.nbt.tag.NumberTag;
 import com.viaversion.viaversion.protocols.v1_18_2to1_19.Protocol1_18_2To1_19;
 import org.checkerframework.checker.nullness.qual.Nullable;
 
 public final class BackwardsMappingData1_19 extends BackwardsMappingData {
-
-    private final boolean sculkShriekerToCryingObsidian = ViaBackwards.getConfig().sculkShriekerToCryingObsidian();
 
     private final Int2ObjectMap<CompoundTag> defaultChatTypes = new Int2ObjectOpenHashMap<>();
 
@@ -40,28 +38,20 @@ public final class BackwardsMappingData1_19 extends BackwardsMappingData {
 
     @Override
     protected void loadExtras(final CompoundTag data) {
-        super.loadExtras(data);
-
-        if (sculkShriekerToCryingObsidian) {
-            blockMappings.setNewId(850, 750);
+        if (ViaBackwards.getConfig().sculkShriekerToCryingObsidian()) {
             for (int i = 18900; i <= 18907; i++) {
                 blockStateMappings.setNewId(i, 16082);
             }
+            itemMappings.setNewId(329, 1065);
         }
+
+        super.loadExtras(data);
 
         final ListTag<CompoundTag> chatTypes = BackwardsMappingDataLoader.INSTANCE.loadNBT("chat-types-1.19.1.nbt").getListTag("values", CompoundTag.class);
         for (final CompoundTag chatType : chatTypes) {
             final NumberTag idTag = chatType.getNumberTag("id");
             defaultChatTypes.put(idTag.asInt(), chatType);
         }
-    }
-
-    @Override
-    public int getNewItemId(final int id) {
-        if (sculkShriekerToCryingObsidian && id == 329) {
-            return 1065;
-        }
-        return super.getNewItemId(id);
     }
 
     public @Nullable CompoundTag chatType(final int id) {
