6b49719e8f2928727f7c20480e7349b785a5be09
FlorianMichael <florian.michael07@gmail.com>
2025-06-29 07:10:21 +0200

    Don't inline type instance into protocol

--- common/src/main/java/com/viaversion/viabackwards/protocol/v1_21_6to1_21_5/storage/ClickEvents.java
+++ common/src/main/java/com/viaversion/viabackwards/protocol/v1_21_6to1_21_5/storage/ClickEvents.java
@@ -19,7 +19,6 @@ package com.viaversion.viabackwards.protocol.v1_21_6to1_21_5.storage;
 
 import com.viaversion.nbt.tag.CompoundTag;
 import com.viaversion.nbt.tag.StringTag;
-import com.viaversion.nbt.tag.Tag;
 import com.viaversion.viabackwards.protocol.v1_21_6to1_21_5.Protocol1_21_6To1_21_5;
 import com.viaversion.viabackwards.protocol.v1_21_6to1_21_5.data.Dialog;
 import com.viaversion.viabackwards.protocol.v1_21_6to1_21_5.provider.DialogViewProvider;
@@ -27,9 +26,7 @@ import com.viaversion.viaversion.api.Via;
 import com.viaversion.viaversion.api.connection.StorableObject;
 import com.viaversion.viaversion.api.connection.UserConnection;
 import com.viaversion.viaversion.api.protocol.packet.PacketWrapper;
-import com.viaversion.viaversion.api.type.Type;
 import com.viaversion.viaversion.api.type.Types;
-import com.viaversion.viaversion.api.type.types.misc.TagType;
 import com.viaversion.viaversion.libs.fastutil.objects.Object2ObjectArrayMap;
 import com.viaversion.viaversion.libs.fastutil.objects.Object2ObjectMap;
 import com.viaversion.viaversion.protocols.v1_21_5to1_21_6.packet.ServerboundPackets1_21_6;
@@ -38,7 +35,6 @@ import java.util.UUID;
 
 public final class ClickEvents implements StorableObject {
 
-    private static final Type<Tag> PAYLOAD_TYPE = new TagType.LengthPrefixedTagType(65536);
     private final Object2ObjectMap<String, CompoundTag> clickEvents = new Object2ObjectArrayMap<>();
 
     public String storeClickEvent(final CompoundTag clickEvent) {
@@ -82,7 +78,7 @@ public final class ClickEvents implements StorableObject {
 
             final PacketWrapper customClickAction = PacketWrapper.create(ServerboundPackets1_21_6.CUSTOM_CLICK_ACTION, connection);
             customClickAction.write(Types.STRING, id);
-            customClickAction.write(PAYLOAD_TYPE, payload);
+            customClickAction.write(Types.CUSTOM_CLICK_ACTION_TAG, payload);
 
             customClickAction.sendToServer(Protocol1_21_6To1_21_5.class);
         }
