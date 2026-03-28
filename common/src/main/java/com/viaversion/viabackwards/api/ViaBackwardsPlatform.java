57157fd5212cd67460483f50a1d94b3e0dce586d
Nassim Jahnke <nassim@njahnke.dev>
2026-03-28 17:06:18 +0100

    Bump version to 5.8.1-SNAPSHOT

--- common/src/main/java/com/viaversion/viabackwards/api/ViaBackwardsPlatform.java
+++ common/src/main/java/com/viaversion/viabackwards/api/ViaBackwardsPlatform.java
@@ -79,7 +79,7 @@ import java.util.logging.Logger;
 
 public interface ViaBackwardsPlatform {
 
-    String MINIMUM_VV_VERSION = "5.8.0";
+    String MINIMUM_VV_VERSION = "5.8.1";
 
     default void init(final File configFile) {
         init(new ViaBackwardsConfig(configFile, getLogger()));
