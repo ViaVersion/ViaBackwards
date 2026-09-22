4bb1b681e60b4b6c769439ea83c100a2f7dd76c2
Rocco <hey@rocco.dev>
2026-09-22 19:58:28 +0000

    Add potion color mappings for new 1.19.4 colors (#1329)

--- common/src/main/java/com/viaversion/viabackwards/protocol/v1_11to1_10/data/SplashPotionMappings1_10.java
+++ common/src/main/java/com/viaversion/viabackwards/protocol/v1_11to1_10/data/SplashPotionMappings1_10.java
@@ -22,27 +22,39 @@ import com.viaversion.viaversion.libs.fastutil.ints.Int2IntOpenHashMap;
 
 public class SplashPotionMappings1_10 {
 
-    private static final Int2IntMap DATA = new Int2IntOpenHashMap(14, 0.99F);
+    private static final Int2IntMap DATA = new Int2IntOpenHashMap(26, 0.99F);
 
     static {
         DATA.defaultReturnValue(-1);
-        DATA.put(2039713, 5); // night vision
-        DATA.put(8356754, 7); // invisibility
-        DATA.put(2293580, 9); // jump boost
-        DATA.put(14981690, 12); // fire resistance
-        DATA.put(8171462, 14); // swiftness
-        DATA.put(5926017, 17); // slowness
-        DATA.put(3035801, 19); // water breathing
-        DATA.put(16262179, 21); // instant health
-        DATA.put(4393481, 23); // instant damage
-        DATA.put(5149489, 25); // poison
-        DATA.put(13458603, 28); // regeneration
-        DATA.put(9643043, 31); // strength
-        DATA.put(4738376, 34); // weakness
-        DATA.put(3381504, 36); // luck
+        DATA.put(0x1F1FA1, 5); // night vision
+        DATA.put(0xC2FF66, 5); // night vision 1.19.4
+        DATA.put(0x7F8392, 7); // invisibility
+        DATA.put(0xF6F6F6, 7); // invisibility 1.19.4
+        DATA.put(0x22FF4C, 9); // jump boost
+        DATA.put(0xFDFF84, 9); // jump boost 1.19.4
+        DATA.put(0xE49A3A, 12); // fire resistance
+        DATA.put(0xFF9900, 12); // fire resistance 1.19.4
+        DATA.put(0x7CAFC6, 14); // swiftness
+        DATA.put(0x33EBFF, 14); // swiftness 1.19.4
+        DATA.put(0x5A6C81, 17); // slowness
+        DATA.put(0x8BAFE0, 17); // slowness 1.19.4
+        DATA.put(0x2E5299, 19); // water breathing
+        DATA.put(0x98DAC0, 19); // water breathing 1.19.4
+        DATA.put(0xF82423, 21); // instant health
+        DATA.put(0x430A09, 23); // instant damage
+        DATA.put(0xA9656A, 23); // instant damage 1.19.4
+        DATA.put(0x4E9331, 25); // poison
+        DATA.put(0x87A363, 25); // poison 1.19.4
+        DATA.put(0xCD5CAB, 28); // regeneration
+        DATA.put(0x932423, 31); // strength
+        DATA.put(0xFFC700, 31); // strength 1.19.4
+        DATA.put(0x484D48, 34); // weakness
+        DATA.put(0x339900, 36); // luck
+        DATA.put(0x59C106, 36); // luck 1.19.4
     }
 
     public static int getOldData(int data) {
-        return DATA.get(data);
+        // newer servers send as ARGB
+        return DATA.get(data & 0xFFFFFF);
     }
 }
